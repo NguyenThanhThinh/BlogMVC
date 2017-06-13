1a38b1f79dbcff16bbcceedbe6d5615191b244ee
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-06-13 15:40:46 +0700

    add controller tag

---
+++ BlogMVC/BlogMVC/Controllers/TagController.cs
@@ -0,0 +1,37 @@
+﻿using BlogMVC.Models;
+using PagedList;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace BlogMVC.Controllers
+{
+    public class TagController : Controller
+    {
+        // GET: Tag
+        public ActionResult List(int? id, int? page)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                // Get articles from database
+                var articles = database.Tags
+                    .Include(t => t.Articles.Select(a => a.Tags))
+                    .Include(t => t.Articles.Select(a => a.Author))
+                    .FirstOrDefault(t => t.Id == id)
+                    .Articles;
+
+                int pageSize = 3;
+                int pageNumber = (page ?? 1);
+
+                // Return the view
+                return View(articles.ToPagedList(pageNumber, pageSize));
+            }
+        }
+    }
+}
